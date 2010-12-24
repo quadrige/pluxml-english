8cd88567e961a117f7f8e098b8e5549105398161
Vincent Lannurien <the.addikt1ve@gmail.com>
2010-12-24 09:01:47 +0100

    moved and translated theme/en.php

---
+++ theme/en/en.php
@@ -0,0 +1,51 @@
+<?php
+
+$LANG = array(
+
+#header.php
+'GOTO_CONTENT'			=> 'Go to content',
+'GOTO_MENU'				=> 'Go to menu',
+'COMMENTS_ATOM_FEEDS'	=> 'Comments\' Atom feed',
+'COMMENTS_RSS_FEEDS'	=> 'Comments\' RSS feed',
+'COMMENTS'				=> 'Comments',
+'ARTICLES_ATOM_FEEDS'	=> 'Articles\' Atom feed',
+'ARTICLES_RSS_FEEDS'	=> 'Articles\' RSS feed',
+'ARTICLES'				=> 'Articles',
+
+# sidebar.php
+'CATEGORIES' 			=> 'Categories',
+'LAST_ARTICLES'			=> 'Latest articles',
+'LAST_COMMENTS'			=> 'Latest comments',
+'ARCHIVES'				=> 'Archives',
+
+# footer.php
+'POWERED_BY'			=> 'Powered by',
+'PLUXML_DESCRIPTION'	=> 'Database-free blog or CMS',
+'IN'					=> 'in',
+'ADMINISTRATION'		=> 'Administration',
+'GOTO_TOP'				=> 'Go to the top',
+'TOP'					=> 'Top',
+
+# erreur.php
+'ERROR'					=> 'An error occurred.',
+'BACKTO_HOME'			=> 'Back to home',
+
+# common
+'WRITTEN_BY'			=> 'Written by',
+'CLASSIFIED_IN'			=> 'Folded in',
+'TAGS'					=> 'Tags',
+
+# commentaires.php
+'SAID'					=> 'said',
+'WRITE_A_COMMENT'		=> 'Write a comment',
+'NAME'					=> 'Name',
+'WEBSITE'				=> 'Website (optional)',
+'EMAIL'					=> 'Email (optional)',
+'COMMENT'				=> 'Comment',
+'CLEAR'					=> 'Clear',
+'SEND'					=> 'Send',
+'COMMENTS_CLOSED'		=> 'Comments are closed.',
+'ANTISPAM_WARNING'		=> 'Antispam verification',
+);
+
+?>
